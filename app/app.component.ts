622c7371f01f712239aaa92b0f01ba41f058ba80
Steve-Burton <stevevburton@gmail.com>
2017-01-18 18:56:58 -0800

    add initial files, folders

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>This is an h1 headline</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
